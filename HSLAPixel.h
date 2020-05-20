9667491b4966f2085cf15f386d4fda4fb743d21e
abj12798 <42579691+abj12798@users.noreply.github.com>
2020-05-20 10:39:18 +0530

    Add files via upload

---
+++ HSLAPixel.h
@@ -0,0 +1,25 @@
+/**
+ * @file HSLAPixel.h
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+namespace uiuc {
+
+  // Put your HSLAPixel class definition here.
+  // (Remember to end it with a semicolon!)
+class HSLAPixel{
+	public:
+		double h;// it is for hue varies between 0-360
+		double l;//	it is for the lumenicance varies between 0-1
+		double s;// it is for the saturation varies between 0-1
+		double a;// it is for the alpha number aries between 0-1
+	};
+
+}
